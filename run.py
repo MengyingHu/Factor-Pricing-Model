263254866c3e02547ade74a204a1eb7f2ce49dde
MengyingHu <zxbsjjesZZ93>
2017-08-02 17:25:01 +0200

    beta

--- run.py
+++ run.py
@@ -10,12 +10,14 @@ from statistics import Statistics
 from settings import path_data, filename
 
 def main():
+    path = path_data
 
-    path_datafile = os.path.join(path_data, filename + ".xlsx")
-
-    portfolio = Statistics.create(path_datafile=path_datafile, sheet_var='VaR', sheet_mr='SP500', sheet_rf='rf',
-                                  sheet_mv='MV (capital)', sheet_bm='PE', sheet_stocks='total return',
-                                  sort_column='VAR REGULATIONS [%]')
+    portfolio = Statistics.create(path_data=path, sheet_var='VaR', sheet_mr='S&P500', sheet_rf='rf',
+                                  sheet_mv='m_v', sheet_bm='MB', sheet_stocks='total return',
+                                  sort_column='Overall Climate Change Resilience Level [%]')
+    beta = portfolio.beta()
+    info = portfolio.correlation()
+    info = portfolio.two_variables_metric(portfolio.book_to_market, var)
 
 if __name__ == "__main__":
     main()
