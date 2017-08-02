a93718d365ac036118051a5812bfc7ea7d4c8f5e
MengyingHu <zxbsjjesZZ93>
2017-08-03 00:26:01 +0200

    beta

--- run.py
+++ run.py
@@ -2,22 +2,23 @@
 import os
 import pandas as pd
 
+# Now, this should work:
 import sys
 sys.path.append("/home/hum/Python_projects/Thesis_Factor_pricing_model/Factor-Pricing-Model")
-# Now, this should work:
 
 from statistics import Statistics
-from settings import path_data, filename
+from settings import path_data
 
 def main():
     path = path_data
 
-    portfolio = Statistics.create(path_data=path, sheet_var='VaR', sheet_mr='S&P500', sheet_rf='rf',
-                                  sheet_mv='m_v', sheet_bm='MB', sheet_stocks='total return',
+    portfolio = Statistics.create(path_data=path, sheet_var='VaR', sheet_mr='MSCI', sheet_rf='rf',
+                                  sheet_mv='MV', sheet_bm='MB', sheet_stocks='RI',
                                   sort_column='Overall Climate Change Resilience Level [%]')
     beta = portfolio.beta()
     info = portfolio.correlation()
     info = portfolio.two_variables_metric(portfolio.book_to_market, var)
+    import ipdb;ipdb.set_trace()
 
 if __name__ == "__main__":
     main()
