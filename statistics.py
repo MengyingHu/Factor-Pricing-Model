8667a2baa6f35a88aed765d60b817fbc916a4208
MengyingHu <zxbsjjesZZ93>
2017-08-08 09:25:13 +0200

    beta

--- statistics.py
+++ statistics.py
@@ -32,7 +32,7 @@ class Statistics():
         # data is time by ISIN
         # ToDo: pay attention to columns and rows
         # pay attention to difference between read_csv and read_excel, for read_csv, the data format should be same
-        var_data = pd.read_csv(os.path.join(path_data, sheet_var + ".csv"), delimiter=';', index_col=0)
+        var_data = pd.read_csv(os.path.join(path_data, sheet_var + ".csv"), delimiter=';', index_col=0)[sort_column]
         # dataframe, time by index
         market_return_data = pd.read_csv(os.path.join(path_data, sheet_mr + ".csv"), delimiter=';', index_col=0)
         # dataframe, time by type
@@ -40,14 +40,19 @@ class Statistics():
         market_value_data = pd.read_csv(os.path.join(path_data, sheet_mv + ".csv"), delimiter=';', index_col=0)
         book_to_market_data = pd.read_csv(os.path.join(path_data, sheet_bm + ".csv"), delimiter=';', index_col=0)
         stocks_total_return_index_data = pd.read_csv(os.path.join(path_data, sheet_stocks + ".csv"), delimiter=';', index_col=0)
-        import ipdb；ipdb.set_trace()
+
+        # convert 'Index' to 'PeriodIndex'
+        market_value_data.index = pd.to_datetime(market_value_data.index)
+        market_return_data.index = pd.to_datetime(market_return_data.index)
+        stocks_total_return_index_data.index = pd.to_datetime(stocks_total_return_index_data.index)
+        book_to_market_data.index = pd.to_datetime(book_to_market_data.index)
+
         # compute periodical monthly data
         #market_value = (market_value_data[:-1] + market_value_data[1:].values) / 2
         market_value = market_value_data[:-1]
         #book_to_market = (book_to_market_data[:-1] + book_to_market_data[1:].values) / 2
         book_to_market = book_to_market_data[:-1]
         market_return = market_return_data[1:] / market_return_data[:-1].values - 1
-        market_return.index = market_value.index
         total_return = stocks_total_return_index_data[1:] / stocks_total_return_index_data[:-1].values - 1
         total_return.index = market_value.index
 
@@ -58,8 +63,9 @@ class Statistics():
         #frame = [total_return, market_value, book_to_market]
         #df = pd.concat(frame, keys=['total return', 'market value', 'book to market'])
         var_data.dropna(axis=0, how='any', inplace=True)
-        total_return = total_return[var_data.index]
+        var_data = var_data.loc[total_return.columns]
         total_return.dropna(axis=1, how='any', inplace=True)
+        var_data = var_data.loc[total_return.columns]
         market_value = market_value[total_return.columns].fillna(method='bfill')
         book_to_market = book_to_market[total_return.columns].fillna(method='bfill')
 
@@ -93,7 +99,13 @@ class Statistics():
        beta = pd.DataFrame([sm.OLS(self.firm_excess_return[col].values, X).fit().params[1] for col in self.total_return.columns], index=self.total_return.columns)
        return beta
 
-    @cached_property
+    def Mkt(self):
+        '''
+        :return: Mkt is dataframe, time by excess return
+        '''
+        # in dataframe subtract, the latter dataframe should be values
+        return (self.market_return - self.rf.values)
+
     def SMB(self):
         '''
         :return: SMB is dataframe, time by smb
@@ -110,10 +122,9 @@ class Statistics():
         #                  for index in self.df.index], index=self.df.index)
         return SMB
 
-    @cached_property
     def HML(self):
         '''
-        :return: HML is dataframe, hml by time
+        :return: HML is dataframe, time by hml
         '''
         n = int(np.ceil(0.3 * self.total_return.columns.shape[0]))
         m = int(np.ceil(0.7 * self.total_return.columns.shape[0]))
@@ -126,10 +137,9 @@ class Statistics():
                         for index in self.total_return.index], index = self.total_return.index)
         return HML
 
-    @cached_property
     def VAR(self):
         '''
-        :return: var is dataframe, var return by time
+        :return: var is dataframe, time by var return
         '''
         n = int(np.ceil(0.25 * self.total_return.columns.shape[0]))
         m = int(np.ceil(0.75 * self.total_return.columns.shape[0]))
@@ -147,15 +157,6 @@ class Statistics():
         '''
         :param factor: one of 'Mkt', 'SMB', 'HML', 'VAR'
         '''
-        if factor == 'SMB':
-            factor = self.SMB
-        elif factor == 'HML':
-            factor = self.HML
-        elif factor == 'VAR':
-            factor = self.VAR
-        elif factor == 'Mkt':
-            factor = self.excess_return
-
         mean = factor.mean()
         std = factor.std()
         min = factor.min()
@@ -164,9 +165,13 @@ class Statistics():
         information = pd.DataFrame([mean, std, min, max], index=['mean', 'std', 'min', 'max'])
         return information
 
-    def correlation(self):
-        import ipdb;ipdb.set_trace()
-        frame = [self.excess_return, self.SMB, self.HML, self.VAR]
+    def correlation(self, factor1, factor2, factor3, factor4):
+        '''
+
+        :param factors: frame of factors, like[Mkt, SMB, HML, VAR]
+        :return:
+        '''
+        frame = [factor1, factor2, factor3, factor4]
         df = pd.concat(frame,axis=1)
         corr_matrix = df.corr()
         return corr_matrix
@@ -179,17 +184,14 @@ class Statistics():
         :return: table to output return sorted by variables
         '''
         # ToDo: check self.variables
-
-        df = self.total_return.T
+        df = self.total_return
         # ToDo: check  if variable_1.columns ==variable_2.columns
         # time constant variable sorted
-        df[str(variable_2)] = variable_2.T
-
-        df = df.sort_values(str(variable_2))
-        del df[str(variable_2)]
+        df.loc['beta'] = variable_2.T.iloc[0]
+        df = df.T.sort_values('beta')
+        del df['beta']
         group_size_2 = len(df) / float(k)
         grouped_2 = df.groupby(np.ceil((np.arange(len(df)) + 1) / group_size_2) - 1)
-
         two_variables_metric = pd.DataFrame({}, index=['beta_low', 'beta_2', 'beta_3', 'beta_4', 'beta_high'],
                                columns=range(k))
         for name, group in grouped_2:
@@ -201,23 +203,52 @@ class Statistics():
                 group_daily[date] = pd.DataFrame([grouped_1[grouped_1.columns[0]].values[size * m:size * (m+1)].mean() for m in range(k)], index=group_daily.index, columns=['beta'])['beta']
                 group_daily[date].loc['beta_high'] = pd.DataFrame([grouped_1[grouped_1.columns[0]].values[size * (k-2):].mean()], columns=['beta'])['beta'].values[0]
             two_variables_metric[name] = group_daily.mean(axis=1)
-
         # time variation variable sorted
         return two_variables_metric.mean(axis=1)
 
-    def firm_specific_regressions(self):
+    def firm_specific_regressions(self, factor1, factor2, factor3, factor4):
         '''
-
+        :param: frame of factors, like[Mkt, SMB, HML, VAR]
         :return: average coefficients of factor regressions
 
         table 2 of 'IS ACCRUALS QUALITY A PRICED RISK FACTOR'
         '''
-        frame = [self.market_excess_return, self.SMB, self.HML, self.VAR]
+        frame = [factor1, factor2, factor3, factor4]
         X = pd.concat(frame, axis=1).values
         X = sm.add_constant(X)
         params = pd.DataFrame([sm.OLS(self.firm_excess_return[col].values, X).fit().params for col in self.total_return.columns], index=self.total_return.columns)
         t_stat = pd.DataFrame([sm.OLS(self.firm_excess_return[col].values, X).fit().tvalues for col in self.total_return.columns], index=self.total_return.columns)
         return params.mean(axis=0), t_stat.mean(axis=0)
+
+    def table_3_size_BM(self, k=5, period_frequency='A'):
+        '''
+
+        :param variable_1:
+        :param variable_2:
+        :return:
+        '''
+        import ipdb;
+        ipdb.set_trace()
+        variable_1 = self.market_value.groupby(pd.TimeGrouper(freq=period_frequency))
+        variable_2 = self.book_to_market.groupby(pd.TimeGrouper(freq=period_frequency))
+        variable_sort = self.total_return.groupby(pd.TimeGrouper(freq=period_frequency))
+
+        params = pd.DataFrame({}, columns=['Mkt', 'SMB', 'HML', 'VAR'])
+        R_2 = pd.DataFrame({}, columns=['R_square'])
+        for date in self.market_value.index:
+            grouped_daily = pd.DataFrame(dict(s1=variable_sort.loc[date], s2=variable_1.loc[date], s3=variable_2.loc[date]))
+            group_sorted_1 = grouped_daily.sort_values('s2')
+            group_size_1 = len(group_sorted_1) / float(k)
+            grouped_1 = group_sorted_1.groupby(np.ceil((np.arange(len(group_sorted_1)) + 1) / group_size_1) - 1)
+            for name, group in grouped_1:
+                import ipdb;
+                ipdb.set_trace()
+                group_sorted_2 = group.sort_values('s3')
+                size = int(len(group_sorted_2) / float(k))
+
+
+
+        return two_variables_metric.mean(axis=1)
 
     #def Fama_MacBeth(self):
 
